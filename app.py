bb8a9484e27f02df07f192e4e4ac26643b8abbb5
Dhrupalsinh <kakashihatake06767@gmail.com>
2026-06-16 10:25:23 +0000

    Initial Flask MySQL Docker Compose application

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"""
+    <h1>DevOps Multi-Container App</h1>
+    <p>Flask Container Running</p>
+    <p>Database Host: {os.getenv('DB_HOST')}</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
